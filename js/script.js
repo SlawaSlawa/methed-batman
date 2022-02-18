6f4a3dd9d354a0f02d87539ac742e39d2dbe4db9
Slawa <slawapocht4@yandex.ru>
2022-02-18 10:34:04 -0800

    day three: added photo.html, video.html & styles

---
+++ js/script.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const sliderThumbs = new Swiper('.slider-thumbs', {
+    loop: true,
+    spaceBetween: 20,
+    slidesPerView: 3,
+    centeredSlides: true,
+    loopedSlides: 4
+})
+
+sliderThumbs.on('click', (swiper) => {
+    swiper.slideTo(swiper.clickedIndex)
+})
+
+const sliderMain = new Swiper('.slider-main', {
+    loop: true,
+    loopedSlides: 4
+})
+
+sliderThumbs.controller.control = sliderMain
+sliderMain.controller.control = sliderThumbs
