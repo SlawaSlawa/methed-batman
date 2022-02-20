faedbe693ad22bb68a46961e665ae4939c2ac138
Slawa <slawapocht4@yandex.ru>
2022-02-20 08:12:49 -0800

    day four: added scripts

--- js/script.js
+++ js/script.js
@@ -1,21 +1,76 @@
 'use strict'
+// BurgerMenu
+const burger = document.querySelector('.burger')
+const navigation = document.querySelector('.navigation')
+const navigationClose = document.querySelector('.navigation__close')
 
-const sliderThumbs = new Swiper('.slider-thumbs', {
-    loop: true,
-    spaceBetween: 20,
-    slidesPerView: 3,
-    centeredSlides: true,
-    loopedSlides: 4
+burger.addEventListener('click', () => {
+    navigation.classList.add('navigation_active')
 })
 
-sliderThumbs.on('click', (swiper) => {
-    swiper.slideTo(swiper.clickedIndex)
+navigationClose.addEventListener('click', () => {
+    navigation.classList.remove('navigation_active')
 })
 
-const sliderMain = new Swiper('.slider-main', {
-    loop: true,
-    loopedSlides: 4
-})
+// Music
+const mute = document.querySelector('.mute__checkbox')
+const audio = new Audio('audio/waterTower.mp3')
 
-sliderThumbs.controller.control = sliderMain
-sliderMain.controller.control = sliderThumbs
+try {
+    const checkMute = () => {
+        if (mute.checked) {
+            audio.play().catch(() => {
+                mute.checked = false
+            })
+        } else {
+            audio.pause()
+        }
+    }
+
+    mute.addEventListener('change', checkMute)
+
+    checkMute()
+} catch {
+    console.log('На этой странице нет музыки')
+}
+
+// Sliders
+try {
+    const sliderThumbs = new Swiper('.slider-thumbs', {
+        loop: true,
+        spaceBetween: 20,
+        slidesPerView: 3,
+        centeredSlides: true,
+        loopedSlides: 4
+    })
+
+    sliderThumbs.on('click', (swiper) => {
+        swiper.slideTo(swiper.clickedIndex)
+    })
+
+    const sliderMain = new Swiper('.slider-main', {
+        loop: true,
+        loopedSlides: 4
+    })
+
+    sliderThumbs.controller.control = sliderMain
+    sliderMain.controller.control = sliderThumbs
+
+    const videos = document.querySelectorAll('video')
+
+    sliderMain.on('slideChange', () => {
+        for (let i = 0; i < videos.length; i++) {
+            videos[i].pause()
+        }
+    })
+
+    const pagination = document.querySelector('.pagination')
+    const paginationButton = document.querySelector('.pagination__arrow')
+
+    paginationButton.addEventListener('click', () => {
+        pagination.classList.toggle('pagination_active')
+    })
+
+} catch {
+    console.log('На этой странице нет слайдера')
+}
